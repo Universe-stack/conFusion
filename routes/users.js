bb012602c41f0df15a1b21db8c91df013ffc754f
Kachi <justicechinedu156@gmail.com>
2023-04-03 04:20:39 +0100

    mongoose population

--- routes/users.js
+++ routes/users.js
@@ -26,13 +26,28 @@ router.post('/signup', (req,res,next)=> {
     res.json({err:err});
   }
   else{
-      passport.authenticate('local')(req,res, ()=>{
-        passport.authenticate('local')(req,res, ()=> {
-          res.statusCode =200;
+    if(req.body.firstname)
+      user.firstname = req.body.firstname;
+
+      if(req.body.lastname)
+      user.lastname = req.body.lastname;
+
+      user.save((err,user)=>{
+        if(err){
+          res.statusCode =500,
           res.setHeader('Content-Type', 'application/json');
-          res.json({success: true, status: 'Registration Successful', user:user })
-        })
+          res.json({err:err});
+          return;
+        }
+        passport.authenticate('local')(req,res, ()=>{
+          passport.authenticate('local')(req,res, ()=> {
+            res.statusCode =200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({success: true, status: 'Registration Successful', user:user })
+          });
+        });
       })
+      
     }
   })
 })
@@ -41,8 +56,7 @@ router.post('/signup', (req,res,next)=> {
 
 
 //login
-router.post('/login', passport.authenticate('local'),(req, res,user)=>{
-          
+router.post('/login', passport.authenticate('local'),(req, res,user)=>{      
     var token = authenticate.getToken({_id: req.user._id});
     res.statusCode =200;
     res.setHeader('Content-Type', 'application/json');
